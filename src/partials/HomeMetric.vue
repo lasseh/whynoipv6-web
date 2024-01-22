b562006f0d7afa86690332664aac94ba80fbbcdb
Lasse Haugen <haugen.lasse@gmail.com>
2024-01-22 13:53:47 +0100

    Update HomeMetric.vue with new data properties

--- src/partials/HomeMetric.vue
+++ src/partials/HomeMetric.vue
@@ -4,22 +4,22 @@
       <div class="grid md:grid-cols-4 bg-gray-800 divide-y md:divide-y-0 md:divide-x divide-gray-700 px-6 md:px-0 md:py-4 text-center">
         <!-- 1st item -->
         <div class="py-6 md:py-0 md:px-8">
-          <div class="text-4xl font-bold leading-tight tracking-tighter text-fuchsia-700 mb-2" data-aos="fade-up">{{ formatLargeNumber(totalsData[0].data.total_sites) }}</div>
+          <div class="text-4xl font-bold leading-tight tracking-tighter text-fuchsia-700 mb-2" data-aos="fade-up">{{ formatLargeNumber(totalsData[0].data.domains) }}</div>
           <div class="text-lg text-gray-400" data-aos="fade-up" data-aos-delay="200">Total valid domains</div>
         </div>
         <!-- 3rd item -->
         <div class="py-6 md:py-0 md:px-8">
-          <div class="text-4xl font-bold leading-tight tracking-tighter text-fuchsia-700 mb-2" data-aos="fade-up">{{ formatLargeNumber(totalsData[0].data.total_ns) }}</div>
+          <div class="text-4xl font-bold leading-tight tracking-tighter text-fuchsia-700 mb-2" data-aos="fade-up">{{ formatLargeNumber(totalsData[0].data.nameserver) }}</div>
           <div class="text-lg text-gray-400" data-aos="fade-up" data-aos-delay="200">IPv6 Enabled Nameservers</div>
         </div>
         <!-- 2nd item -->
         <div class="py-6 md:py-0 md:px-8">
-          <div class="text-4xl font-bold leading-tight tracking-tighter text-fuchsia-700 mb-2" data-aos="fade-up">{{ formatLargeNumber(totalsData[0].data.total_aaaa) }}</div>
+          <div class="text-4xl font-bold leading-tight tracking-tighter text-fuchsia-700 mb-2" data-aos="fade-up">{{ formatLargeNumber(totalsData[0].data.base_domain) }}</div>
           <div class="text-lg text-gray-400" data-aos="fade-up" data-aos-delay="200">IPv6 Enabled domains</div>
         </div>
         <!-- 4rd item -->
         <div class="py-6 md:py-0 md:px-8">
-          <div class="text-4xl font-bold leading-tight tracking-tighter text-fuchsia-700 mb-2" data-aos="fade-up">{{ formatLargeNumber(totalsData[0].data.total_both) }}</div>
+          <div class="text-4xl font-bold leading-tight tracking-tighter text-fuchsia-700 mb-2" data-aos="fade-up">{{ formatLargeNumber(totalsData[0].data.heroes) }}</div>
           <div class="text-lg text-gray-400" data-aos="fade-up" data-aos-delay="200">Fully IPv6 Ready Domains</div>
         </div>
       </div>
