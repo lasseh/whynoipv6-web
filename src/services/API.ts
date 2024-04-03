7f2078129737f0722c564844973443188ffe3b19
Lasse Haugen <haugen.lasse@gmail.com>
2024-04-03 10:51:57 +0200

    Update API default URL

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default (url = "https://api.whynoipv6.com/") => {
-// export default (url = "https://api.ipv6.fail/") => {
-// export default (url = "http://localhost:9001/") => {
+  // export default (url = "https://api.ipv6.fail/") => {
+  // export default (url = "http://localhost:9001/") => {
   return axios.create({
     baseURL: url,
     headers: {
