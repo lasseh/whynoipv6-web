546dd65798346fb2e0a71afa739ca6f79e58961b
Lasse Haugen <haugen.lasse@gmail.com>
2024-11-26 08:48:11 +0100

    Add tooltips to icon components

--- src/partials/icons/Check.vue
+++ src/partials/icons/Check.vue
@@ -1,5 +1,22 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-  </svg>
+  <div class="has-tooltip relative inline-block">
+    <span class="tooltip absolute rounded border border-slate-700 shadow-lg p-1 bg-gray-800 text-fuchsia-600 normal-case transform -translate-x-1/2 -translate-y-full">Supported</span>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+    </svg>
+  </div>
 </template>
+
+<style scoped>
+.tooltip {
+  visibility: hidden;
+  left: 50%; /* Center horizontally */
+  bottom: -50%; 
+  white-space: nowrap; /* Prevent tooltip text from wrapping */
+}
+
+.has-tooltip:hover .tooltip {
+  visibility: visible;
+  z-index: 50;
+}
+</style>
--- src/partials/icons/Minus.vue
+++ src/partials/icons/Minus.vue
@@ -1,5 +1,22 @@
 <template>
+  <div class="has-tooltip relative inline-block">
+    <span class="tooltip absolute rounded border border-slate-700 shadow-lg p-1 bg-gray-800 text-fuchsia-600 normal-case transform -translate-x-1/2 -translate-y-full">No Records</span>
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15" />
   </svg>
+  </div>
 </template>
+
+<style scoped>
+.tooltip {
+  visibility: hidden;
+  left: 50%; /* Center horizontally */
+  bottom: -50%; 
+  white-space: nowrap; /* Prevent tooltip text from wrapping */
+}
+
+.has-tooltip:hover .tooltip {
+  visibility: visible;
+  z-index: 50;
+}
+</style>
