5a40351cd434e36922c6c7b867debb1f0bc59abd
Lasse Haugen <haugen.lasse@gmail.com>
2024-11-26 14:14:09 +0100

    Add PageIllustrationHex component and update index export

--- src/partials/index.ts
+++ src/partials/index.ts
@@ -1,5 +1,5 @@
 export { default as Header } from "@/partials/Header.vue";
-export { default as PageIllustration } from "@/partials/PageIllustration.vue";
+export { default as PageIllustration } from "@/partials/PageIllustrationHex.vue";
 export { default as Footer } from "@/partials/Footer.vue";
 export { default as CheckIcon } from "@/partials/icons/Check.vue";
 export { default as CrossIcon } from "@/partials/icons/Cross.vue";
